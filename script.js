881fb2890f70fc5be695952e34da7087214fbbd6
saarthack <ssarthak2003@outlook.com>
2025-03-26 21:54:11 +0530

    setTimeout and setInterval

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-var arr = [
-    {
-        team:'CSK',
-        primary:'yellow',
-        secondary:'green'
-    },
-    {
-        team:'RCB',
-        primary:'red',
-        secondary:'black'
-    },
-    {
-        team:'MI',
-        primary:'blue',
-        secondary:'gold'
-    },
-    {
-        team:'KKR',
-        primary:'purple',
-        secondary:'gold'
-    },
-    {
-        team:'SRH',
-        primary:'orange',
-        secondary:'black'
-    }
-]
-
 var btn = document.querySelector('button')
-var h1 = document.querySelector('h1')
+var percent = document.querySelector('#percent')
+var growth = document.querySelector('#growth')
+
+var grow = 0
 
 btn.addEventListener('click',function(){
-    var num = Math.floor(Math.random()*arr.length)
+    var int = setInterval(function(){
+        grow++
+        percent.innerHTML = grow+'%'
+        growth.style.width = grow+'%'
+    },40)
 
-    var winner = arr[num]
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+    }, 4000);
 
-    h1.innerHTML = winner.team
-    h1.style.backgroundColor = winner.primary
-    h1.style.color = winner.secondary
 })
-
-// https://github.com/saarthack/live1
